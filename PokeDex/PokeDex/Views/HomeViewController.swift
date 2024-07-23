586006c860058ccc2cb4729e5afa2562a6fba4c3
giacatano <142597978+giacatano@users.noreply.github.com>
2024-07-23 16:50:21 +0200

    [UI Improvements]

--- PokeDex/PokeDex/Views/HomeViewController.swift
+++ PokeDex/PokeDex/Views/HomeViewController.swift
@@ -9,21 +9,21 @@ import UIKit
 
 class HomeViewController: UIViewController {
     
-    //private lazy var pokemonViewModel = HomeViewModel(pokemonRepository: PokemonRepository(apiHandler: APIHandler()))
+    private lazy var pokemonViewModel = HomeViewModel(pokemonRepository: PokemonRepository(apiHandler: APIHandler()))
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 }
 
-extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
-    }
-    
-
-}
+//extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return 1
+//    }
+//    
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        return UITableViewCell
+//    }
+//    
+//
+//}
