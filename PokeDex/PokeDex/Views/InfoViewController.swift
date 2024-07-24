91babb260db1621d3690b362ee59379579b0fb0a
giacatano <142597978+giacatano@users.noreply.github.com>
2024-07-24 14:47:53 +0200

    [Added Stat IBOutlets]

--- PokeDex/PokeDex/Views/InfoViewController.swift
+++ PokeDex/PokeDex/Views/InfoViewController.swift
@@ -8,7 +8,18 @@
 import UIKit
 
 class InfoViewController: UIViewController {
-
+    
+    @IBOutlet private weak var pokemonCharacterImageView: UIImageView!
+  
+    @IBOutlet private weak var hpLabel: UILabel!
+    @IBOutlet private weak var hpBaseStatLabel: UILabel!
+    
+    @IBOutlet weak var attackLabel: UILabel!
+    @IBOutlet weak var attackStatLabel: UILabel!
+    
+    @IBOutlet weak var defenseLabel: UILabel!
+    @IBOutlet weak var defenseStatLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
