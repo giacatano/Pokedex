3b72ab5a5cf7c2f87e822d2c263257b211577c81
giacatano <142597978+giacatano@users.noreply.github.com>
2024-07-24 14:57:40 +0200

    [Connected Remaining Outlets]

--- PokeDex/PokeDex/Views/InfoViewController.swift
+++ PokeDex/PokeDex/Views/InfoViewController.swift
@@ -10,15 +10,18 @@ import UIKit
 class InfoViewController: UIViewController {
     
     @IBOutlet private weak var pokemonCharacterImageView: UIImageView!
-  
     @IBOutlet private weak var hpLabel: UILabel!
-    @IBOutlet private weak var hpBaseStatLabel: UILabel!
-    
-    @IBOutlet weak var attackLabel: UILabel!
-    @IBOutlet weak var attackStatLabel: UILabel!
-    
-    @IBOutlet weak var defenseLabel: UILabel!
-    @IBOutlet weak var defenseStatLabel: UILabel!
+    @IBOutlet private weak var hpStatLabel: UILabel!
+    @IBOutlet private weak var attackLabel: UILabel!
+    @IBOutlet private weak var attackStatLabel: UILabel!
+    @IBOutlet private weak var defenseLabel: UILabel!
+    @IBOutlet private weak var defenseStatLabel: UILabel!
+    @IBOutlet private weak var specialAttackLabel: UILabel!
+    @IBOutlet private weak var specialAttackStatLabel: UILabel!
+    @IBOutlet private weak var specialDefenseLabel: UILabel!
+    @IBOutlet private weak var specialDefenseStatLabel: UILabel!
+    @IBOutlet private weak var speedLabel: UILabel!
+    @IBOutlet private weak var speedStatLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
