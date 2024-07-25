1be6224647a0f007bf7a948ddd43692c75c6f20e
giacatano <142597978+giacatano@users.noreply.github.com>
2024-07-25 09:33:42 +0200

    [Added Table View]

---
+++ PokeDex/PokeDex/Views/InfoTableViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  InfoTableViewCell.swift
+//  PokeDex
+//
+//  Created by Gia Catano on 2024/07/25.
+//
+
+import UIKit
+
+class InfoTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var statsCategoryLabel: UILabel!
+    @IBOutlet weak var baseStatLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        
+
+    }
+}
--- PokeDex/PokeDex/Views/InfoViewController.swift
+++ PokeDex/PokeDex/Views/InfoViewController.swift
@@ -10,18 +10,8 @@ import UIKit
 class InfoViewController: UIViewController {
     
     @IBOutlet private weak var pokemonCharacterImageView: UIImageView!
-    @IBOutlet private weak var hpLabel: UILabel!
-    @IBOutlet private weak var hpStatLabel: UILabel!
-    @IBOutlet private weak var attackLabel: UILabel!
-    @IBOutlet private weak var attackStatLabel: UILabel!
-    @IBOutlet private weak var defenseLabel: UILabel!
-    @IBOutlet private weak var defenseStatLabel: UILabel!
-    @IBOutlet private weak var specialAttackLabel: UILabel!
-    @IBOutlet private weak var specialAttackStatLabel: UILabel!
-    @IBOutlet private weak var specialDefenseLabel: UILabel!
-    @IBOutlet private weak var specialDefenseStatLabel: UILabel!
-    @IBOutlet private weak var speedLabel: UILabel!
-    @IBOutlet private weak var speedStatLabel: UILabel!
+    @IBOutlet private weak var pokemonCharacterNameLabel: UILabel!
+    @IBOutlet private weak var pokemonStatsTableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
