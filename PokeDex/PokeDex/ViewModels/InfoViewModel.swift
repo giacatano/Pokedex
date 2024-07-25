443e5e3bea5c112f36c97f7d1794bc7c57692fca
giacatano <142597978+giacatano@users.noreply.github.com>
2024-07-25 11:43:07 +0200

    [Fetched Stats]

--- PokeDex/PokeDex/ViewModels/InfoViewModel.swift
+++ PokeDex/PokeDex/ViewModels/InfoViewModel.swift
@@ -8,14 +8,29 @@
 import Foundation
 
 class InfoViewModel {
-    
     let pokemonRepository: PokemonRepositoryType?
     private var stats: [Statistics]
     private weak var delegate: HomeViewModelProtocol?
+    private var pokemonStatsEndpoint: String?
     
     init(pokemonRepository: PokemonRepositoryType?, delegate: HomeViewModelProtocol) {
         self.pokemonRepository = pokemonRepository
         self.stats = []
         self.delegate = delegate
     }
+    
+        func fetchPokemonStats(){
+            pokemonRepository?.fetchPokemonStats(url: pokemonStatsEndpoint ?? "") { [weak self] result in
+                switch result {
+                case .failure(let error):
+                    print(error)
+                    self?.delegate?.showError(error: error)
+                case .success(let pokemonStats):
+                    self?.stats = pokemonStats.stats
+    
+                    self?.delegate?.reloadView()
+                    print(pokemonStats)
+                }
+            }
+        }
 }
